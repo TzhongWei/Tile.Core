b16dca71bd4826658475c02d89c6117d70edb023
TsungWei Cheng <mike861104@gmail.com>
2025-01-03 17:17:09 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using Rhino;
+using Rhino.Geometry;
+
+namespace Tile.Core
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            RhinoApp.WriteLine("Debug in 8");
+        }
+    }
+}
